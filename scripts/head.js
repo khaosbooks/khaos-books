7b0f3d01ccab864dab6b1b1f7faa1a9875a97144
khaosbooks <k.konstantinova13@gmail.com>
2025-04-15 22:21:08 -0400

    cleaning up head code

---
+++ scripts/head.js
@@ -0,0 +1,13 @@
+document.write(`
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Forum&family=Jost:wght@400;500;700&display=swap" rel="stylesheet">
+
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>KhaosBooks</title>
+<link rel="stylesheet" href="styles/base.css">
+<style type="text/css">@import url("https://assets.mlcdn.com/fonts.css?version=1744283");</style>
+  `);
