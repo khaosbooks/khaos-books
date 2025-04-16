48a6a00d2966ef8de0f5b245c697907fa8cbd6fb
khaosbooks <k.konstantinova13@gmail.com>
2025-04-15 23:40:58 -0400

    form test3

--- scripts/head.js
+++ scripts/head.js
@@ -10,4 +10,13 @@ document.write(`
 <title>KhaosBooks</title>
 <link rel="stylesheet" href="styles/base.css">
 <style type="text/css">@import url("https://assets.mlcdn.com/fonts.css?version=1744283");</style>
+
+<script>
+    (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1455211');
+</script>
+<!-- End MailerLite Universal -->
   `);
